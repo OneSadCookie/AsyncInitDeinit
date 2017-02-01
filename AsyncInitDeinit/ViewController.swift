f1b3fbaf03f42ed4fb5f194dcb1d720722058ddd
Keith Bauer <onesadcookie@gmail.com>
2017-02-01 17:34:54 +1300

    example

--- AsyncInitDeinit/ViewController.swift
+++ AsyncInitDeinit/ViewController.swift
@@ -1,25 +1,98 @@
-//
-//  ViewController.swift
-//  AsyncInitDeinit
-//
-//  Created by Katie Huber on 1/02/17.
-//  Copyright © 2017 Katie Huber. All rights reserved.
-//
-
 import UIKit
+
+import PromiseKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var cancel: () -> () = {}
+    var model: Model!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        label.isHidden = true
+        cancelButton.isHidden = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cancel(sender: Any?) {
+        cancel()
+        label.isHidden = true
+        cancelButton.isHidden = true
+        startButton.isHidden = false
     }
-
+    
+    @IBAction func start(sender: Any?) {
+        model = nil
+        startButton.isHidden = true
+        let _ = firstly { () -> Promise<Model> in
+            self.label.isHidden = false
+            self.label.text = "Loading A"
+            self.cancelButton.isHidden = false
+            return createAsync(cancel: &self.cancel).then { [unowned self] (a: A) -> Promise<Model> in
+                self.label.text = "Loading B"
+                return createAsync(cancel: &self.cancel).then { [unowned self] (b: B) -> Promise<Model> in
+                    self.label.text = "Loading C"
+                    return createAsync(cancel: &self.cancel).then { (c: C) -> Model in
+                        self.label.isHidden = true
+                        self.cancelButton.isHidden = true
+                        self.startButton.isHidden = false
+                        return Model(a: a, b: b, c: c)
+                    }
+                }
+            }
+        }
+        .then { [unowned self] (model: Model) -> () in
+            self.model = model
+        }
+    }
 
 }
 
+protocol DefaultConstructible {
+    init()
+}
+
+class Thing: DefaultConstructible {
+
+    required init() {
+        print("Finished making \(type(of: self))")
+    }
+
+    deinit {
+        print("Cleaning up \(type(of: self))")
+    }
+    
+}
+
+class A: Thing {}
+class B: Thing {}
+class C: Thing {}
+
+func createAsync<T: DefaultConstructible>(cancel: inout () -> ()) -> Promise<T> {
+    let (cancellationPromise, _, reject) = Promise<T>.pending()
+    cancel = {
+        reject(NSError.cancelledError())
+    }
+    return race(after(interval: 3).then { () -> T in return T() }, cancellationPromise)
+}
+
+class Model {
+
+    var a: A
+    var b: B
+    var c: C
+    
+    init(a: A, b: B, c: C) {
+        self.a = a
+        self.b = b
+        self.c = c
+        print("Created Model")
+    }
+    
+    deinit {
+        print("Destroying Model")
+    }
+
+}
